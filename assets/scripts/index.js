277caa74898c11538b7054647c85918275038b91
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-01-23 16:16:22 -0500

    Update 023/master from browser-template
    Refresh for cohort 023

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,7 +8,7 @@ $(() => {
 })
 
 // use require with a reference to bundle the file and use it in this file
-// const example = require('./example');
+// const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
